00ddb752690109b58a8a5754bdf7046eeaee1dbd
Cody <codysolomon15@gmail.com>
2020-05-13 13:57:53 -0700

    completed project for today

---
+++ schemes/scheme-model.js
@@ -0,0 +1,49 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where("id", id).first()
+}
+
+function findSteps(id) {
+  return db('steps as steps')
+    .where({'steps.scheme_id': id})
+    .join('schemes as schemes', 'steps.scheme_id', 'schemes.id')
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+}
+
+function add(scheme) {
+  return db("schemes").insert(scheme)
+    .then((ids) => {
+      return findById(ids[0])
+    })
+}
+
+function update(changes, id) {
+  return db("schemes").where("id", id).first().update(changes)
+    .then(() => {
+      return findById(id)
+    })
+}
+
+function remove(id) {
+  return findById(id)
+    .then(scheme => {
+      return db("schemes").where({id}).first().del()
+        .then(scheme => {
+          return scheme
+        })
+    })
+}
